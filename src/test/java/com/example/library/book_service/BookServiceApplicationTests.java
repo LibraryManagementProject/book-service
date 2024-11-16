014b1c4ba21e99e1936d80497e4ba88244358df6
bits <2023mt93350@wilp.bits-pilani.ac.in>
2024-11-16 13:13:56 +0530

    adding boilerplate

---
+++ src/test/java/com/example/library/book_service/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library.book_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
